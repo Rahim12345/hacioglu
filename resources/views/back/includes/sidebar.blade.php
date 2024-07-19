4db5b8aba45ac808598968799786b5a14e329143
Rahim Süleymanov <rahim.suleymanov94@gmail.com>
2024-07-19 06:30:09 +0400

    sidebar fixed

--- resources/views/back/includes/sidebar.blade.php
+++ resources/views/back/includes/sidebar.blade.php
@@ -31,25 +31,25 @@
                 </li>
                 <li class="{{ request()->segment(2) == 'pairs' ? 'active-page-link' : '' }}">
                     <a href="{{ route('pairs.index') }}">
-                        <i class="bi bi-house"></i>
+                        <i class="bi bi-mask"></i>
                         <span class="menu-text">Əvvəl-Sonra</span>
                     </a>
                 </li>
                 <li class="{{ request()->segment(2) == 'portfolio' ? 'active-page-link' : '' }}">
                     <a href="{{ route('portfolio.create') }}">
-                        <i class="bi bi-house"></i>
+                        <i class="bi bi-briefcase-fill"></i>
                         <span class="menu-text">İşlərimiz</span>
                     </a>
                 </li>
                 <li class="{{ request()->segment(2) == 'review' ? 'active-page-link' : '' }}">
                     <a href="{{ route('review.create') }}">
-                        <i class="bi bi-house"></i>
+                        <i class="bi bi-megaphone-fill"></i>
                         <span class="menu-text">Rəylər</span>
                     </a>
                 </li>
                 <li class="{{ request()->segment(2) == 'service' ? 'active-page-link' : '' }}">
                     <a href="{{ route('service.create') }}">
-                        <i class="bi bi-house"></i>
+                        <i class="bi bi-person-video2"></i>
                         <span class="menu-text">Xidmətlər</span>
                     </a>
                 </li>
